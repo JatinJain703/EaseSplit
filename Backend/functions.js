dd7f426bd04888401dc46722cc540e1c1f3381c5
Jatin Jain <jain.jatin703@gmail.com>
2025-06-26 23:50:47 +0530

    auth middleware

--- Backend/functions.js
+++ Backend/functions.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const EMAIL = process.env.EMAIL;
 const emailpass = process.env.pass;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 function generateotp() {
   return Math.floor(100000 + Math.random() * 900000);
@@ -16,9 +17,25 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+function auth(req,res,next)
+{
+    const token=req.headers.token;
+    const user=jwt.verify(token,JWT_SECRET);
+    if(!user)
+    {
+        res.status(400).send({
+            message:"You are not logged in!!"
+        })
+    }
+    else{
+        req.user=user.id;
+        next();
+    }
+}
 
 // ✅ Export the functions for CommonJS
 module.exports = {
   generateotp,
-  transporter
+  transporter,
+  auth
 };
